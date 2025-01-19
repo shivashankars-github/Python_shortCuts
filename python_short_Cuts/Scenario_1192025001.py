81d92faa0a5b0dd7d46b937234b2fdc0119747cb
[shiva] <76905310+shivashankars-github@users.noreply.github.com>
2025-01-19 20:03:19 +0530

    'all'

---
+++ python_short_Cuts/Scenario_1192025001.py
@@ -0,0 +1,7 @@
+abcde = ['a', 'b', 'c', 'd', 'e']
+xyzwp = ['x', 'y', 'z', 'w', 'p']
+numlist = ['1', '2', '3', '4', '5']
+
+Result = tuple(map(lambda x, y , z : (x,y,z), abcde,xyzwp,numlist))
+
+print(Result)
